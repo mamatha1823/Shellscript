2c2a069175b7020942d5827391404b3c7063941f
Mamatha kishore <raisingstar8@gmail.com>
2025-01-02 17:20:58 +0530

    running a command in shellscript

---
+++ 09-special-variabkes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "all the variables  passed to the script $@"
+echo "no of variables passed to the script : $#"
+echo "script name : $0"
+echo "working  directory : $PWD"
+echo "pid of the script executing now: $$"
+echo "home dir of current user: $HOME"
+echo "pid of last background process: $!
