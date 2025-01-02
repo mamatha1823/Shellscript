19932bacb2d2e91d6931962952b63edd979ca8a6
Mamatha kishore <raisingstar8@gmail.com>
2025-01-02 14:12:47 +0530

    vaariables

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "enter the username"
+
+read  USERNAME 
+
+echo  "entered username is $USERNAME"
+
+echo "eneter the password"
+
+read  -s password
+
+echo "entered password is $password"
