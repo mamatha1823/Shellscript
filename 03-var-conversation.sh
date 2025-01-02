6e6bfae2d9bcb4acf62393778181a493e877e709
Mamatha kishore <raisingstar8@gmail.com>
2025-01-02 13:19:20 +0530

    var-conv changes

--- 03-var-conversation.sh
+++ 03-var-conversation.sh
@@ -2,6 +2,6 @@
 P1=hansi
 P2=Riyan
 
-echo"Hi $P1, how are you"
-echo"I'm good, $P2 how are you"
-echo" ok $P1, lets go to church"
+echo "Hi $P1, how are you"
+echo "I'm good, $P2 how are you"
+echo " ok $P1, lets go to church"
