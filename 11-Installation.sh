f7d86a5324ad4a846656b5d67d40c4ba627b37f4
Mamatha kishore <raisingstar8@gmail.com>
2025-01-06 12:28:57 +0530

    functions

--- 11-Installation.sh
+++ 11-Installation.sh
@@ -14,8 +14,28 @@ then
   dnf install mysql -y
   if [ $? -ne 0 ]
   then 
-    echo " sql is installation is done"
+   echo " sql is installation is failed"
+   exit 1;
+  else
+   echo "sql is successfully installed"
   fi
 else
    echo "sql is already installed "
 fi
+
+dnf list installed git -y
+if [ $? -ne 0 ]
+then
+   echo "git is not installed , going to install"
+   dnf install git -y
+   if [ $? ne 0 ]
+   then 
+     echo "git is successfully installed"
+   else
+     echo "git installation failed,please check"
+     exit 1
+   fi
+else
+   echo "Already Git is installated"
+fi
+
