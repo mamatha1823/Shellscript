887bad8ddfeec963a6b39dba6b65415da568315b
Mamatha kishore <raisingstar8@gmail.com>
2025-01-08 18:11:29 +0530

    loops-SOFTWARE INSTALLATION

---
+++ 15.loops_s/wINSTALL.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+Check_root(){
+if [ $USER_ID -ne 0]
+then
+ echo -e " $R please run with root it $N"
+ exit 1
+fi
+}
+
+Validate(){
+if [ $1 -ne 0 ]
+then
+    echo - e" $R $2 is FAILED$N" 
+else
+    echo -e " $G $2 is success $N"
+ }
+
+Check_root
+
+for $package in $@
+do
+ dnf  list installed $package
+ if [ $? -ne 0 ]
+ then
+   echo  -e " $R $package is not installed ,going to install  $N"
+   dnf install $package -y
+   Validate $? "Installing $package"
+ fi
+done
