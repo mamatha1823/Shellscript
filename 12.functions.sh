b5d5d9ffb007e588703d7e6effbdf5abb2aa1284
Mamatha kishore <raisingstar8@gmail.com>
2025-01-08 17:29:21 +0530

    functions

--- 12.functions.sh
+++ 12.functions.sh
@@ -7,6 +7,14 @@
  N="\e[0m
  G="\e[32m
 
+ Check_root(){
+if [ $user_id -ne 0 ]
+then
+ echo -e "please run with root access"
+ exit 1
+fi
+}
+
  Validate(){
     if [ $1 -ne 0 ]
     then 
@@ -16,30 +24,27 @@
       echo -e "$G $2   is success $N "
     fi 
     }
-if [ $user_id -ne 0 ]
-then
- echo -e "please run with root access"
- exit 1
-fi
 
-dnf list installed git
+Check_root
 
-if [ $? -ne 0]
-then 
-  echo -e "$R git is not installated $N"
-  dnf install git -y
-  Validate $? "git instalation is"
-else
-  echo -e "$G git is already INSTALLED $N"
-fi
+# dnf list installed git
 
-dnf list installed mysql 
+# if [ $? -ne 0]
+# then 
+#   echo -e "$R git is not installated $N"
+#   dnf install git -y
+#   Validate $? "git instalation is"
+# else
+#   echo -e "$G git is already INSTALLED $N"
+# fi
 
-if [ $? -ne 0 ]
-then 
- echo -e "$R mysql is not installed $N"
- dnf install git -y
- Validate $? "mysql installation"
-else
- echo -e " $G git is already INSTALLED $N "
-fi
+# dnf list installed mysql 
+
+# if [ $? -ne 0 ]
+# then 
+#  echo -e "$R mysql is not installed $N"
+#  dnf install git -y
+#  Validate $? "mysql installation"
+# else
+#  echo -e " $G git is already INSTALLED $N "
+# fi
