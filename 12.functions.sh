2ee24c9beb6d6e25ec3f116c0cfffad9a7e586dc
Mamatha kishore <raisingstar8@gmail.com>
2025-01-08 11:57:53 +0530

    functions

---
+++ 12.functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ user_id=$(id -u)
+ V=$variables
+
+ Validate(){
+    if [ $1 -ne 0 ]
+    then 
+     echo -e "$R $2 .... is not  installed, please install $N"
+     dnf install $2
+     if [ $1 -ne 0 ]
+     then 
+      echo -e "$R  $2 installation failed, please check "
+      exit 1
+     else
+      echo -e "$G $2 INSTALLATION  is success, please check "
+     fi
+
+ }
+dnf installed list $V
+Validate $? $V
