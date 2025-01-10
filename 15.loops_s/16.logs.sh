886a914be336947396caad2028dea4118bdb9dd5
Mamatha kishore <raisingstar8@gmail.com>
2025-01-10 11:23:49 +0530

    logs

---
+++ 15.loops_s/16.logs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+USER_ID-$(id -u)
+Log_folder="/var/log/shell-script"
+script_name=$(echo $0 | cut -d '.' -f1)
+time=%Y-%m-%d-%H-%M-%S
+log_file="$Log_folder/$script_name-$time.log"
+mkdir -p $Log_folder
+
+
+##colours
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+check_root(){
+   if [$USER_ID -ne 0 ]
+   then
+     echo -e " $R please switch to root ID  to proceed $N" &>>log_file
+     exit 1
+}
+
+check_root
+
+Validate(){
+    if [ $1 -ne 0]
+    then 
+      echo -e "$R $2... is Failed $N" &>>log_file
+      exit 1
+    else
+      echo -e "$G  $2.. IS Success $N" &>>log_file
+}
+
+
+for package in $@
+do
+  dnf list installed $package
+  if [ $? -ne 0 ]
+  then 
+     echo -e "$R $package is not installed, going to install $N" &>>log_file
+     dnf install $package
+     Validate $? "$package installation"
+  else
+     echo -e "$G $package is already installed, nothing to do $N" &>>log_file
+  fi
+done
+
