6b57ea81b7f82e2a59fe4aa8c43e6b8865924dd1
Mamatha kishore <raisingstar8@gmail.com>
2025-01-10 12:06:37 +0530

    logs

--- 15.loops_s/16.logs.sh
+++ 15.loops_s/16.logs.sh
@@ -15,7 +15,7 @@ N="\e[0m"
 check_root(){
    if [ $USER_ID -ne 0 ]
    then
-     echo -e " $R please switch to root ID  to proceed $N" &>>$log_file
+     echo -e " $R please switch to root ID  to proceed $N"  | tee &>>$log_file
      exit 1
    fi
 }
@@ -23,37 +23,37 @@ check_root(){
 check_root
 
 Usage(){
-     echo -e "$R usage is  package1, package2....$N"
+     echo -e "$R usage is  package1, package2....$N"   | tee &>>$log_file
  
+}
+
+
+Validate(){
+    if [ $1 -ne 0 ]
+    then 
+      echo -e "$R $2... is Failed $N" | tee &>>$log_file
+      exit 1
+    else
+      echo -e "$G  $2.. IS Success $N" | tee  &>>$log_file
+    fi
 }
 
 
 if [ $# -eq 0 ]
 then
  Usage
-fi
-
-Validate(){
-    if [ $1 -ne 0 ]
-    then 
-      echo -e "$R $2... is Failed $N" &>>$log_file
-      exit 1
-    else
-      echo -e "$G  $2.. IS Success $N" &>>$log_file
-    fi
-}
-
+fi 
 
 for package in $@
 do
   dnf list installed $package
   if [ $? -ne 0 ]
   then 
-     echo -e "$R $package is not installed, going to install $N" &>>$log_file
+     echo -e "$R $package is not installed, going to install $N" |tee &>>$log_file
      dnf install $package
      Validate $? "$package installation"
   else
-     echo -e "$G $package is already installed, nothing to do $N" &>>$log_file
+     echo -e "$G $package is already installed, nothing to do $N"  | tee &>>$log_file
   fi
 done
 
