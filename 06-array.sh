b128cc4c92cd0241fbc75eb354e7130ce273ea41
Mamatha kishore <raisingstar8@gmail.com>
2025-04-07 19:30:08 +0530

    change

--- 06-array.sh
+++ 06-array.sh
@@ -5,5 +5,5 @@
 #!/bin/bash
 Fruits=("apple" "Kiwi" "Orange" "Melon")
 echo "1st fruit is $Fruits[0]"
-echo "total of elements is $#
-echo "all fruits $Fruits[@] "
+echo "total of elements is $#"
+echo "all fruits $Fruits[@]"
