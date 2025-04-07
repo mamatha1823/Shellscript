92927b7fb6434a6b65b825a775fa06d7ed19fef8
Mamatha kishore <raisingstar8@gmail.com>
2025-04-07 18:33:57 +0530

    change

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,13 +1,21 @@
+# #!/bin/bash
+
+# # echo "enter the username"
+
+# read  USERNAME #read command interupts and takes input into variabe
+
+# echo  "entered username is $USERNAME"
+
+# echo "eneter the password"
+
+# read  -s password
+
+# echo "entered password is $password"
+
 #!/bin/bash
-
-echo "enter the username"
-
-read  USERNAME #read command interupts and takes input into variabe
-
-echo  "entered username is $USERNAME"
-
-echo "eneter the password"
-
-read  -s password
-
-echo "entered password is $password"
+echo "enter username "
+read USERNAME
+echo "entered Username is  $USERNAME "
+echo "enter password"
+read -s PASSWORD
+echo "entered password is $PASSWORD"
