57407465d106768c4bbb5132fd71b93ca3a8a57f
Mamatha kishore <raisingstar8@gmail.com>
2025-04-09 16:39:01 +0530

    change

--- 08-data-Types.sh
+++ 08-data-Types.sh
@@ -2,11 +2,11 @@
 
 #!/bin/bash
 Num1=$1
-echo "enter Num1"
-read Num1
+# echo "enter Num1"
+# read Num1
 Num2=$2
-echo "enter Num2"
-read Num2
-sum=$(Num1+Num2)
+# echo "enter Num2"
+# read Num2
+sum=(Num1+Num2)
 echo "total sum is {$($Num1+$Num2)} "
 echo "total sum is $sum "
