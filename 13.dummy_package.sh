7c3491c1fad1418f2232f3a88ef5462ae57dd183
Mamatha kishore <raisingstar8@gmail.com>
2025-04-11 00:55:15 +0530

    logs trial

--- 13.dummy_package.sh
+++ 13.dummy_package.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+dnf list installed -y
+
 dnf install no -y
 if [ $? -ne 0 ]
 then
- echo " installation failed"
+ echo " installation success"
  exit 1
 else
- echo "installation is success"
+ echo "installation is failed"
 fi
