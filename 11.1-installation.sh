e6486f048520f7bd5c11ab0f23b0442b30a9495f
Mamatha kishore <raisingstar8@gmail.com>
2025-04-10 16:05:52 +0530

    2nd trial

--- 11.1-installation.sh
+++ 11.1-installation.sh
@@ -15,7 +15,7 @@ check_user(){
 }
     
 Validate(){
-    if ($1 -ne 0)
+    if [$1 -ne 0]
     then
      echo -e " $ $R $2 is failed $N" 
      exit
@@ -26,7 +26,7 @@ Validate(){
 check_user
 
 dnf list installed mysql
-if ($? -ne 0)
+if [$? -ne 0]
 then
  dnf install mysql -y
  Validate $? "Installing mysql"
