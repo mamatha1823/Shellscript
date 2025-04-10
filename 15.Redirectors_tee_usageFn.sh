cfd1352ea9082b98993e85211094572b8ad08308
Mamatha kishore <raisingstar8@gmail.com>
2025-04-11 01:33:06 +0530

    tee_usage_Func

---
+++ 15.Redirectors_tee_usageFn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+user_id=$(id -u)
+Log_file=/var/log/shellscript
+mkdir -p Log_file
+time=$(date +%y-%m-%D-%H-%M-%s)
+script_name=$(echo "$0 | cut -d -name "*"
+log_file=$(Log_file/$script_name-$time.log # format of the log file 
+
+COLOURS ## for highlighting the statements
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+Check_root(){
+ if [ $user_id -ne 0]
+ then 
+    echo "run with root"
+    exit 1
+ fi
+ }
+
+ Usage(){
+  if [ $# eq 0 ]
+  then 
+    echo " usage is like { $0 package1 package2 ....} "
+    exit1
+  done
