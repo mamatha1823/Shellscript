f1c424cff5894b77ccc856cd60c81b66dde817c4
Mamatha kishore <raisingstar8@gmail.com>
2025-04-11 02:38:31 +0530

    tee_usage_Func

--- 15.Redirectors_tee_usageFn.sh
+++ 15.Redirectors_tee_usageFn.sh
@@ -23,10 +23,10 @@ Check_root(){
 Usage(){
   if [ $# eq 0 ]
   then 
-    echo -e " $R usage: sudo sh $0 package1 package2 ....$N "
+    echo -e " $R usage: sudo sh $0 package1 package2 ....$N "  | tee -a $log_file
+    exit 1
   fi
 }
-
 
 Check_root
 Usage
